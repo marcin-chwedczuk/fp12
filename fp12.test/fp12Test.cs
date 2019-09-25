8c8d6f7046c75b76a84860af0d64ca4cc7754ca7
Marcin Chwedczuk <chwedczukm@yahoo.com>
2019-09-25 16:25:58 +0200

    Fix errors in fp12 <-> float conversion

--- fp12.test/fp12Test.cs
+++ fp12.test/fp12Test.cs
@@ -38,12 +38,66 @@ namespace fp12test
             Assert.True(x.is_negative_infinity);
         }
 
-        [Fact]
-        public void conv_to_float() {
-            fp12 x = (fp12)3.1415f;
-            float fx = (float)x;
+        public class conversions_between_float_and_fp12 {
 
-            Assert.Equal(3.1415f, fx);
+            [Fact]
+            public void can_convert_finite_value() {
+                fp12 x = (fp12)3.1415f;
+                float fx = (float)x;
+
+                // TODO: Check this is legit
+                Assert.Equal(3.140625f, fx);
+            }
+    
+            [Fact]
+            public void can_convert_min_positive_value() {
+                fp12 x = fp12.MIN_POSITIVE_VALUE;
+                
+                float fx = (float)x;
+
+                Assert.Equal(6.103515625E-05, fx);
+            }
+ 
+            [Fact]
+            public void can_convert_max_positive_value() {
+                fp12 x = fp12.MAX_POSITIVE_VALUE;
+                
+                float fx = (float)x;
+
+                Assert.Equal(255f, fx);
+            }
+
+            [Fact]
+            public void zero_converts_to_zero() {
+                fp12 x = fp12.ZERO;
+
+                float fx = (float)x;
+
+                Assert.Equal(0.0f, fx);
+            }
         }
-      }
+
+        public class equality_operators {
+            [Fact]
+            public void equals_works() {
+                fp12 pi = (fp12)3.14159;
+                fp12 pi2 = (fp12)3.14159;
+                fp12 e = (fp12)2.7183;
+
+                Assert.True(pi == pi2);
+                Assert.True(e == e);
+
+                Assert.False(e == pi);
+                Assert.False(pi == e);
+
+                Assert.False(pi == fp12.NaN);
+                Assert.False(fp12.NaN == fp12.NaN);
+
+                Assert.True(fp12.POSTIVE_INFINITY == fp12.POSTIVE_INFINITY);
+                Assert.False(fp12.POSTIVE_INFINITY == fp12.NEGATIVE_INFINITY);
+
+                Assert.True(fp12.ZERO == fp12.ZERO);
+            }
+        }
+     }
 }
--- fp12/fp12lib/fp12.cs
+++ fp12/fp12lib/fp12.cs
@@ -33,21 +33,21 @@ namespace fp12lib {
 
         public fp12(ushort value) { __value = value; }
 
-        private const ushort SIGN_MASK = 0b00001000_00000000;
+        private const ushort SIGN_MASK = 0b0000_1000__0000_0000;
         public int __sign => 
             ((__value & SIGN_MASK) >> (EXPONENT_BIT_COUNT + MANTISSA_BIT_COUNT));
 
         internal const int SIGN_POSITIVE = 0;
         internal const int SIGN_NEGATIVE = 1;
 
-        public const ushort EXPONENT_MASK = 0b00000111_10000000;
+        public const ushort EXPONENT_MASK = 0b0000_0111__1000_0000;
         public int __exponent =>
             ((__value & EXPONENT_MASK) >> MANTISSA_BIT_COUNT);
 
         public int __unbiased_exponent =>
             (__exponent - BIAS);
 
-        public const ushort MANTISSA_MASK = 0b00000000_01111111;
+        public const ushort MANTISSA_MASK = 0b0000_0000__0111_1111;
         public int __mantissa =>
             (__value & MANTISSA_MASK);
 
@@ -68,29 +68,93 @@ namespace fp12lib {
             (__mantissa == 0) &&
             (__sign == SIGN_NEGATIVE);
 
+        public bool is_denormalized =>
+            (__exponent < EXPONENT_MIN) &&
+            (__mantissa != 0);
+
         public static readonly fp12 NaN = new fp12(0b00000111_11111111);
         public static readonly fp12 POSTIVE_INFINITY = new fp12(0b00000111_10000000);
         public static readonly fp12 NEGATIVE_INFINITY = new fp12(0b00001111_10000000);
+
+        public static readonly fp12 MAX_POSITIVE_VALUE = new fp12(0b0000_0111__0111_1111);
+        public static readonly fp12 MIN_POSITIVE_VALUE = new fp12(0b0000_0000__0000_0001);
+
+        public static readonly fp12 ZERO = new fp12(0b0000_0000__0000_0000);
+
+        public static bool operator ==(fp12 left, fp12 right) {
+            if (left.is_nan || right.is_nan) return false;
+
+            // TODO: +0 == -0
+            if (left.__value == right.__value) return true;
+
+            // +0.0 == -0.0
+            if (left.__value == 0 && right.__value == 0b0000_1000__0000_0000) {
+                return true;
+            }
+
+            // -0.0 == +0.0
+            if (left.__value == 0b0000_1000__0000_0000 && right.__value == 0) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool operator !=(fp12 left, fp12 right) {
+            if (left.is_nan || right.is_nan) return false;
+
+            return !(left == right);
+        }
 
         // Conversion fp12 -> float
         public static explicit operator float(fp12 value) {
             // TODO: Implement infinities and NaNs
 
+            if (value.is_nan) { return float.NaN; }
+            if (value.is_positive_infinity) { return float.PositiveInfinity; }
+            if (value.is_negative_infinity) { return float.NegativeInfinity; }
+            if (value == ZERO) { return 0.0f; }
+
+            uint fMantissa = ((uint)value.__mantissa) << (float_bytes.MANTISSA_BIT_COUNT - MANTISSA_BIT_COUNT);
+            int fUnbiasedExponent = value.__unbiased_exponent;
+
+            if (value.is_denormalized) {
+                // Shift mantissa left untill we get a 1 at the
+                // begining to get a normalized value.
+                // Float has much broader range so there
+                // should be no problem with exponend out of range.
+                // TODO: Check above
+
+                uint m = (uint) value.__mantissa;
+                while ((m & (1u << MANTISSA_BIT_COUNT)) == 0) {
+                    m <<= 1;
+                    fUnbiasedExponent--;
+                }
+
+                // Clear top '1' that is implicit.
+                m &= ~(1u << MANTISSA_BIT_COUNT);
+
+                fMantissa = m;
+            }
+
             float f = new float_bytes()
                 .with_sign((uint) value.__sign)
-                .with_unbiased_exponent(value.__unbiased_exponent)
-                .with_mantissa(((uint)value.__mantissa) << (float_bytes.MANTISSA_BIT_COUNT - MANTISSA_BIT_COUNT))
+                .with_unbiased_exponent(fUnbiasedExponent)
+                .with_mantissa(fMantissa)
                 .to_float();
 
             return f;
         }
 
         public static explicit operator fp12(float f) {
-            // TODO: Infs and NaN
+            if (float.IsNaN(f)) { return NaN; }
+            if (float.IsPositiveInfinity(f)) { return POSTIVE_INFINITY; }
+            if (float.IsNegativeInfinity(f)) { return NEGATIVE_INFINITY; }
 
+            
             var fb = new float_bytes(f);
 
-
+            // TODO: Denormalized values
             // TODO: Check out of range
 
             ushort fp12 = 0;
@@ -109,6 +173,10 @@ namespace fp12lib {
             fp12 |= (ushort)mantissa;
 
             return new fp12(fp12);
+        }
+
+        public static fp12 operator +(fp12 left, fp12 right) {
+            return fp12.NaN;
         }
     }
 }
