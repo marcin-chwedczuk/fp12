951b13fb4aa34a48ba5e252de5943e41c393aa55
Marcin Chwedczuk <chwedczukm@yahoo.com>
2019-09-26 18:29:47 +0200

    another testing example

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -18,15 +18,19 @@ namespace consoleapp
             for (int i = 0; i < POINTS_COUNT; i++) {
                 float x = -1.0f + i * 2.0f / POINTS_COUNT;
 
-                float c3f = 4*x*x*x - 3*x;
+                // https://en.wikipedia.org/wiki/Chebyshev_polynomials
 
-                fp12 xfp12 = (fp12)x;
-                fp12 c3fp12 = ((fp12)4.0f) *xfp12*xfp12*xfp12 - ((fp12)3.0f)*xfp12;
+                float c5 = 16*pow(x, 5) - 20*pow(x, 3) + 5*x;
 
-                sb.AppendFormat("{0} {1} {2} {3}", x, c3f, (float)xfp12, (float)c3fp12).AppendLine();
+                fp12 X = (fp12)x;
+                fp12 C5 = ((fp12)16.0f)*X*X*X*X*X - ((fp12)20.0f)*X*X*X + ((fp12)5.0f)*X;
+
+                sb.AppendFormat("{0} {1} {2} {3}", x, c5, (float)X, (float)C5).AppendLine();
             }
 
             File.WriteAllText("/home/mc/chart.txt", sb.ToString());
         }
+
+        private static float pow(float f, int n) => (float)Math.Pow(f, n);
     }
 }
